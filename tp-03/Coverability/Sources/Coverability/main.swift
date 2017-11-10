b427a2c95f1b2e6fa2fe10519ea31094df227432
silentmind <besa.saqipi@etu.unige.ch>
2017-11-10 19:09:47 +0100

    tp3

--- tp-03/Coverability/Sources/Coverability/main.swift
+++ tp-03/Coverability/Sources/Coverability/main.swift
@@ -13,3 +13,19 @@ import CoverabilityLib
 // You **are** encouraged to write tests of your own!
 // You may write as many tests as you want here, or even better in `CoverabilityLibTests.swift`.
 // Observe how the two existing tests are implemented to write your own.
+
+let model = createUnboundedModel()
+guard let s0 = model.places.first(where: {$0.name == "s0"}),
+      let s1 = model.places.first(where: {$0.name == "s1"}),
+      let s2 = model.places.first(where: {$0.name == "s2"}),
+      let s3 = model.places.first(where: {$0.name == "s3"}),
+      let s4 = model.places.first(where: {$0.name == "s4"}),
+      let b  = model.places.first(where: {$0.name == "b"})
+    else {
+        fatalError("Invalid model")
+}
+
+let initialMarking: CoverabilityMarking = [s0: 1, s1: 0, s2: 1, s3: 0, s4: 1, b: 0]
+let coverabilityGraph = model.coverabilityGraph(from: initialMarking)
+
+print(coverabilityGraph.count)
--- tp-03/Coverability/Sources/CoverabilityLib/PTNet+Extensions.swift
+++ tp-03/Coverability/Sources/CoverabilityLib/PTNet+Extensions.swift
@@ -12,8 +12,85 @@ public extension PTNet {
         // IMPORTANT: Your function MUST return a valid instance of CoverabilityGraph! The optional
         // print debug information you'll write in that function will NOT be taken into account to
         // evaluate your homework.
+        let transitions = self.transitions
+        let firstNode = CoverabilityGraph(marking: marking)
+        var toVisit = [CoverabilityGraph]()
+        var visitedGraphs = [CoverabilityGraph]()
 
-        return CoverabilityGraph(marking: marking)
+        // Initial node is the first visited node
+        toVisit.append(firstNode)
+
+        while(!toVisit.isEmpty) {
+          let currentGraph = toVisit.remove(at:0)
+          visitedGraphs.append(currentGraph)
+
+          // fire each fireable transition and process the new generated markings
+          for t in transitions {
+            if var newMark = t.fire(from: currentGraph.marking) {
+              newMark = omegaAdd(newMark, visitedGraphs)
+              if let alreadyVisitedGraph = visitedGraphs.first(where: {$0.marking == newMark}) {
+                currentGraph.successors[t] = alreadyVisitedGraph
+              }
+              else {
+                let discoveredGraph = CoverabilityGraph(marking: newMark)
+                currentGraph.successors[t] = discoveredGraph
+
+                if (!toVisit.contains(where: {discoveredGraph.marking > $0.marking})) {
+                  if (!toVisit.contains(where: {$0.marking == discoveredGraph.marking})) {
+                    toVisit.append(discoveredGraph)
+                  }
+                }
+              }
+            }
+          }
+        }
+        return firstNode
+      }
     }
 
-}
+    public extension PTTransition {
+      public func isFireable(from marking: CoverabilityMarking) -> Bool {
+        for a in self.preconditions {
+          if case .some(let n) = marking[a.place]! {
+            if n < a.tokens {
+              return false
+            }
+          }
+        }
+        return true
+      }
+
+      public func fire(from marking: CoverabilityMarking) -> CoverabilityMarking? {
+        guard self.isFireable(from: marking) else {
+          return nil
+        }
+        var result = marking
+
+        for a in self.preconditions {
+          if case .some(let n) = result[a.place]! {
+            result[a.place]! = .some(n - a.tokens)
+          }
+        }
+        for a in self.postconditions {
+          if case .some(let n) = result[a.place]! {
+            result[a.place]! = .some(n + a.tokens)
+          }
+        }
+        return result
+      }
+    }
+
+    fileprivate func omegaAdd(_ marking: CoverabilityMarking, _ graphs: [CoverabilityGraph]) -> CoverabilityMarking {
+      var ret = marking
+      for pastNode in graphs {
+        if ret > pastNode.marking{
+          for place in ret.keys {
+            if ret[place]! > pastNode.marking[place]! {
+              ret[place] = .omega
+            }
+          }
+          return ret
+        }
+      }
+      return marking
+    }
