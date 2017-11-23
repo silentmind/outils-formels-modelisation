f1fd2267f21b7ee4d9e979d95918ed3b25128145
silentmind <besa.saqipi@etu.unige.ch>
2017-11-23 23:27:54 +0100

    tp-04

--- tp-04/Philosophers/Sources/Philosophers/main.swift
+++ tp-04/Philosophers/Sources/Philosophers/main.swift
@@ -2,56 +2,33 @@ import PetriKit
 import PhilosophersLib
 
 do {
-    enum C: CustomStringConvertible {
-        case b, v, o
-
-        var description: String {
-            switch self {
-            case .b: return "b"
-            case .v: return "v"
-            case .o: return "o"
-            }
-        }
-    }
-
-    func g(binding: PredicateTransition<C>.Binding) -> C {
-        switch binding["x"]! {
-        case .b: return .v
-        case .v: return .b
-        case .o: return .o
-        }
-    }
-
-    let t1 = PredicateTransition<C>(
-        preconditions: [
-            PredicateArc(place: "p1", label: [.variable("x")]),
-        ],
-        postconditions: [
-            PredicateArc(place: "p2", label: [.function(g)]),
-        ])
-
-    let m0: PredicateNet<C>.MarkingType = ["p1": [.b, .b, .v, .v, .b, .o], "p2": []]
-    guard let m1 = t1.fire(from: m0, with: ["x": .b]) else {
-        fatalError("Failed to fire.")
-    }
-    print(m1)
-    guard let m2 = t1.fire(from: m1, with: ["x": .v]) else {
-        fatalError("Failed to fire.")
-    }
-    print(m2)
+    let philosophers = lockFreePhilosophers(n: 5)
+    let philosophersMarking = philosophers.markingGraph(from : philosophers.initialMarking!)
+    print("For the unlockable Philosophers model with 5 philosophers, there are \(philosophersMarking!.count) possible markings.\n")
 }
-
-print()
 
 do {
-    let philosophers = lockFreePhilosophers(n: 3)
-    // let philosophers = lockablePhilosophers(n: 3)
-    for m in philosophers.simulation(from: philosophers.initialMarking!).prefix(10) {
-        print(m)
+  let philosophers = lockablePhilosophers(n: 5)
+  let philosophersMarking = philosophers.markingGraph(from : philosophers.initialMarking!)
+  print("For the lockable Philosophers model with 5 philosophers, there are \(philosophersMarking!.count) possible markings.\n")
+
+  lock : for node in philosophersMarking! {
+    var isFound = true
+    for (_, e) in node.successors {
+      if e.count != 0 {
+        isFound = false
+      }
     }
+    if isFound {
+      print("There's a locked case with the marking \(node.marking).")
+      break lock
+    }
+  }
 }
 
-/* Exercice fait en classe le 17 octobre
+
+/*
+--Exercice fait en classe le 17 octobre--
 do {
   // dom(Ingredients) = {p, t, m}
   enum Ingredients {
